429ff1eb31d26218bbd2a40bc49dd3d8f22bbae4
GabrielKlose18 <51412891+GabrielKlose18@users.noreply.github.com>
2019-10-25 17:33:04 -0300

    Reorganizando código

--- main.cs
+++ main.cs
@@ -4,94 +4,32 @@ using System.Text;
 
 namespace Contratar{
   class MainClass {
-  public static void Main (string[] args) {
+    public static void Main (string[] args) {
+     string opcaoPerfil;
+     string opcaoUsuario;
 
-    StreamReader Autenticador;
-    StreamWriter DataEmpreendedores;
-    StreamWriter DataCompradores;
+      Console.WriteLine ("Bem vindo ao InvestCity!");
+      Console.WriteLine("Desenvolvendo relações!");
+      Console.WriteLine("Qual o seu Perfil? ");
+      Console.WriteLine("Digite: \n 1 - Empreendedor \n 2 - Comprador");
+      opcaoPerfil = Console.ReadLine();
 
-    int opcao;
-
-    Console.WriteLine ("Bem vindo ao InvestCity!");
-    Console.WriteLine("Desenvolvendo relações!");
-    Console.WriteLine("Possui cadastro conosco? ");
-    Console.WriteLine("Digite: \n 1 - SIM \n 2 - NÃO");
-    opcao = Console.ReadLine();
-
-    if(opcao = 1){
-      Autenticador = File.OpenText("usuarios.txt");
-            Console.WriteLine("Confirme seu telefone cadastrado: \n");
-            CadUser.Telefone = Console.ReadLine();
-
-            while(Autenticador.EndOfStream != true){
-              string linha = Autenticador.ReadLine();
-              if(linha.Contains(CadUser.Telefone)){
-                Console.WriteLine("Usuário encontrado");
-              }
-    else if(opcao = 2){
-
-      int Cad;
-      Console.WriteLine("Precisamos realizar seu cadastro: ");
-      Console.WriteLine("Escolha: 1 - Empreendedor \n 2 - Comprador");
-      Cad = Console.ReadLine();
-
-      if(Cad = 1){
-
-        string vetEmpreendedor[,] = new string[100,100];
-
-
-        DataEmpreendedores = File.AppendText("servicos.txt");
-
-        Console.WriteLine("Nome do Empreendedor: ");
-        Empreendedor.Nome = Console.ReadLine();
-        DataEmpreendedores.WriteLine(Empreendedor.Nome);
-        Console.WriteLine("Telefone para Contato: ");
-        Empreendedor.Telefone = Console.ReadLine();
-        DataEmpreendedores.WriteLine(Empreendedor.Telefone);
-        Console.WriteLine("Tipo de serviço: ");
-        Empreendedor.tipoServico = Console.ReadLine();
-        DataEmpreendedores.WriteLine(Empreendedor.tipoServico);
-        Console.WriteLine("Cidade da prestação de serviços: ");
-        Empreendedor.Cidade = Console.ReadLine();
-        DataEmpreendedores.WriteLine(Empreendedor.Cidade);
-
-        //falta fazer alguma coisa para armazenar o preco num vetor
-
-        vetEmpreendedor[i] = DataEmpreendedores;
-      }
-
-      else if(Cad = 2){
-
-        string vetCompradores[,] = new string[100,100];
-
-
-        DataCompradores = File.AppendText("contratantes.txt");
-
-        Console.WriteLine("Nome do Comprador: ");
-        Comprador.Nome = Console.ReadLine();
-        DataCompradores.WriteLine(Comprador.Nome);
-        Console.WriteLine("Telefone para Contato: ");
-        Comprador.Telefone = Console.ReadLine();
-        DataCompradores.WriteLine(Comprador.Telefone);
-        Console.WriteLine("Cidade do Comprador: ");
-        DataCompradores.cidade = Console.ReadLine();
-        DataCompradores.WriteLine(Comprador.cidade);
-        
-
-        //falta fazer alguma coisa para armazenar o preco num vetor
-
-        vetCompradores[i] = DataCompradores;
-      }
-
-      else:
-        while(Cad != 1 && Cad != 2){
-          Console.WriteLine("Opção inválida! Digite um número válido: ");
-          Cad = Console.ReadLine();
+      if(opcaoPerfil = "1"){
+        Autenticador = File.OpenText("./BD/empreendedor.txt");
+        Console.WriteLine("Possui cadastro conosco? ");
+        Console.WriteLine("Digite: \n 1 - SIM \n 2 - NÃO");
+        opcaoUsuario = Console.ReadLine();
+        if(opcaoUsuario = "1"){
+          while(Autenticador.EndOfStream != true){
+            string linha = Autenticador.ReadLine();
+            if(linha.Contains(CadUser.Telefone)){
+              Console.WriteLine("Usuário encontrado");
+            }
+          }
         }
-        
-    }          
+      }
+      
     }
   }
-}
 }
 
